32d91633940f4b3ade78d0ca88c453ecaaeca796
vilyever <250450035@163.com>
2016-08-05 14:52:58 +0800

    Initial Commit

---
+++ objcString/objcString.h
@@ -0,0 +1,19 @@
+//
+//  objcString.h
+//  objcString
+//
+//  Created by FTET on 16/8/5.
+//  Copyright © 2016年 vilyever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for objcString.
+FOUNDATION_EXPORT double objcStringVersionNumber;
+
+//! Project version string for objcString.
+FOUNDATION_EXPORT const unsigned char objcStringVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <objcString/PublicHeader.h>
+
+
